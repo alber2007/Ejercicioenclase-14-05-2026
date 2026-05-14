e6b393b03e898d1f7cb3077eea6234b06e55bfb0
Alberto Madrid <albeto.madrid@udla.edu.ec>
2026-05-14 15:01:43 -0500

    segundo commit del ejercicio de punteros

---
+++ ejercicio2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    int numeros [7] = {3, 6, 9, 12, 15, 18, 21};
+    int *ptr;
+    ptr = &numeros[0];
+
+    for (int i = 0; i < 7; i++) {
+
+    printf("%d\n", numeros [i]);
+    printf("%p\n",ptr);
+    }
+
+    return 0; 
+}
