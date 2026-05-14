d00d5a913e74e305e4442dc71713aa0f8d268591
Laboratorio <laboratorio@udla-ec.int>
2026-05-14 14:49:07 -0500

    Primer commit

---
+++ ejercicio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    
+    int numeros [7] = {3, 6, 9, 12, 15, 18, 21};
+    for (int i = 0; i < 7; i++) {
+
+    printf("%d\n", numeros [i]);
+    }
+
+    return 0; 
+}
